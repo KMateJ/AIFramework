da6621469bf98de5e1c0222460b93bf4a177830d
P6FT0Y <matejozsef.kocsis@gmail.com>
2023-11-30 12:22:07 +0100

    simple cost function

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-float Train[][2 ]={
+double Train[][2]={
     {0,0},
     {1,2},
     {2,4},
@@ -12,9 +12,31 @@ float Train[][2 ]={
     {5,10},
 };
 
-int main(){
+#define train_count (int)(sizeof(Train)/sizeof(Train[0]))
 
-    printf("Hello World\n");
+double rand_float(void){
+    return (double) rand() / (double) RAND_MAX;
+}
+
+
+double cost( double w){
+    double res = 0.0f;
+    for (int i = 0; i < train_count ; ++i){
+        double x = Train[i][0];
+        double y = x*w;
+
+        res += (y-Train[i][1])*(y-Train[i][1]);
+    }
+
+    return res/train_count;
+}
+
+int main(){
+    srand(40);
+
+    double w = rand_float()*100;
+
+    printf ("cost: %f", cost(w));
 
     return 0;
 }
