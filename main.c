2ade88899278c8f783e3a6d02f85fd53da1aea14
P6FT0Y <matejozsef.kocsis@gmail.com>
2023-11-30 11:33:07 +0100

    Init

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+float Train[][2 ]={
+    {0,0},
+    {1,2},
+    {2,4},
+    {3,6},
+    {4,8},
+    {5,10},
+};
+
+int main(){
+
+    printf("Hello World\n");
+
+    return 0;
+}
