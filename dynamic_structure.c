e767901193ec83cda6266c804e3418dd4c4c0cf3
P6FT0Y <matejozsef.kocsis@gmail.com>
2024-01-21 23:29:04 +0100

    Init matrix functions

--- dynamic_structure.c
+++ dynamic_structure.c
@@ -12,19 +12,53 @@ double sigf(double x){
 typedef struct {
     size_t rows;
     size_t cols;
-    double *e;
+    double *es;
 
 }Mat;
 
 Mat init_mat(size_t r, size_t c){
+    Mat matrix ;
 
+    matrix.rows = r,
+    matrix.cols = c;
+    matrix.es = malloc(sizeof(*matrix.es)*r*c);
+
+    return matrix;
+}
+
+void mat_dot(Mat dst, Mat A, Mat B){
+
+}
+
+void mat_sum(Mat dst, Mat A){
+
+}
+
+void mat_print(Mat m){
+    for (size_t i = 0; i< m.rows; ++i){
+        for (size_t j = 0; j< m.cols; ++j) {
+            printf("%.3f ",m.es[i*m.cols + j]);
+        }
+        printf("\n");
+    }
 }
 
 
 
 int main(){
 
-    printf("Fuckin awsome\n");
+
+    int r =10;
+    int c =9;
+    Mat m = init_mat(r,c);
+
+    for (int i=1;i<r*c+1;++i){
+        m.es[i-1] = i;
+    }
+
+    mat_print(m);
+
+    free(m.es);
 
     return 0;
 }
