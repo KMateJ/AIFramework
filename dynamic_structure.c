a1b21761a5e5c3c3c25442543f780d663adfe37c
P6FT0Y <matejozsef.kocsis@gmail.com>
2024-01-22 14:38:33 +0100

    Implementing matrix functions

--- dynamic_structure.c
+++ dynamic_structure.c
@@ -4,10 +4,16 @@
 #include <math.h>
 
 
+#define MAT_AT(m,i,j) (m).es[(i)*(m).cols + (j)]
+
 double sigf(double x){
     return 1.f/(1.f+expf(-x));
 }
 
+
+double rand_double(){
+    return (double) rand () / (double) RAND_MAX; 
+}
 
 typedef struct {
     size_t rows;
@@ -28,10 +34,44 @@ Mat init_mat(size_t r, size_t c){
 
 void mat_dot(Mat dst, Mat A, Mat B){
 
+    if (A.cols != B.rows) {printf("The given matrixes are not compatible\n")   ;return;}
+    if (A.rows != dst.rows || B.cols != dst.cols) {printf("The given dst matrix is not compatible\n")   ;return;}
+
+    for ( size_t i; i < A.rows; ++i ){
+        for (size_t j; j < B.cols; ++j ){
+            MAT_AT(dst,i,j) = 0;
+            for (size_t k; k < A.cols; ++k){
+                MAT_AT(dst,i,j) += MAT_AT(A,i,k)*MAT_AT(B,k,j);
+            }
+        }
+    }
 }
 
 void mat_sum(Mat dst, Mat A){
 
+    if (dst.cols != A.cols || dst.rows != A.rows) {printf("The given matrixes are not compatible\n")   ;return;}
+
+    for (int i=0; i< dst.rows; ++i) {
+        for (int j=0; j< dst.cols; ++j) {
+            MAT_AT(dst,i,j) += MAT_AT(A,i,j);
+        }
+    }
+}
+
+void mat_rand( Mat m, double low, double high){
+    for (size_t i = 0; i< m.rows; ++i){
+        for (size_t j = 0; j< m.cols; ++j) {
+            MAT_AT(m,i,j) = rand_double() * (high - low) + low;
+        }
+    }
+}
+
+void mat_fill(Mat m, double value){
+    for (size_t i = 0; i< m.rows; ++i){
+        for (size_t j = 0; j< m.cols; ++j) {
+            MAT_AT(m,i,j) = value;
+        }
+    }
 }
 
 void mat_print(Mat m){
@@ -47,15 +87,18 @@ void mat_print(Mat m){
 
 int main(){
 
+    srand(time(NULL));
 
-    int r =10;
-    int c =9;
+    int r =5;
+    int c =5;
     Mat m = init_mat(r,c);
+    Mat x = init_mat(r,c);
 
-    for (int i=1;i<r*c+1;++i){
-        m.es[i-1] = i;
-    }
+    mat_fill(m,1);
+    mat_fill(x,1);
 
+    mat_print(m);
+    mat_sum(m,x);
     mat_print(m);
 
     free(m.es);
